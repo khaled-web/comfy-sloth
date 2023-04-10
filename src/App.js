6089a1634812b1084572cf13f03f8a0c16f0b07b
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-04-10 14:16:44 +0200

    react-router...done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import { 
+ Home,
+ About,
+ AuthWrapper,
+ Cart,
+ Checkout,
+ Error,
+ PrivateRoute,
+ Product,
+ SingleProduct} from './pages'
+
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return(
+    // <Router>
+    //   <Navbar/>
+    //   <Sidebar/>
+    //   <Switch>
+    //     <Route exact path='/'>
+    //       <Home/>
+    //     </Route>
+
+    //     <Route exact path='/about'>
+    //       <About/>
+    //     </Route>
+
+    //     <Route exact path='/cart'>
+    //       <Cart/>
+    //     </Route>
+
+    //     <Route exact path='/products'>
+    //       <Product/>
+    //     </Route>
+
+    //     <Route exact path='/products/:id' children={SingleProduct}> 
+    //     </Route>
+
+    //     <Route exact path='/checkout'>
+    //       <Checkout/>
+    //     </Route>
+
+    //     <Route exact path='*'>
+    //       <Error/>
+    //     </Route>
+
+    //   </Switch>
+    //   <Footer/>
+    // </Router>
+    <BrowserRouter>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/products' element={<Product/>}/>
+        <Route path='/products/:id' element={<SingleProduct/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='*' element={<Error/>}/>
+
+
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from './HomePage.js'
+import About from './AboutPage.js'
+import AuthWrapper from './AuthWrapper.js'
+import Cart from './CartPage.js'
+import Checkout from './CheckoutPage.js'
+import Error from './ErrorPage.js'
+import PrivateRoute from './PrivateRoute.js'
+import Product from './ProductsPage.js'
+import SingleProduct from './SingleProductPage.js'
+
+export {
+ Home,
+ About,
+ AuthWrapper,
+ Cart,
+ Checkout,
+ Error,
+ PrivateRoute,
+ Product,
+ SingleProduct
+}
