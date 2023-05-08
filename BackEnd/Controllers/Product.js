fb088528aa898522bb58acba0b82966574703879
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-05-08 15:01:48 +0200

    Product-ControllersFunction-Done

--- BackEnd/Controllers/Product.js
+++ BackEnd/Controllers/Product.js
@@ -1,7 +1,11 @@
 //............
 //importing
 //............
-
+const Product = require('../models/product.js')
+const {
+ StatusCodes
+} = require('http-status-codes')
+const CustomError = require('../errors')
 
 
 //............
@@ -9,27 +13,76 @@
 //............
 //create product
 const createProduct = async (req, res) => {
- res.send('create product')
+ req.body.user = req.user.userId
+ const product = await Product.create(req.body)
+ res.status(StatusCodes.CREATED).json({
+  product
+ })
 }
 
 //getAllProducts
 const getAllProducts = async (req, res) => {
- res.send('get all products')
+ const product = await Product.find({})
+ res.status(StatusCodes.OK).json({
+  count: product.length,
+  product
+ })
 }
 
 //getSingleProduct
 const getSingleProduct = async (req, res) => {
- res.send('get single product')
+ const {
+  id: productId
+ } = req.params
+ const product = await Product.findOne({
+  _id: productId
+ })
+ if (!product) {
+  throw new CustomError.NotFoundError(`No product with id: ${productId}`)
+ }
+ res.status(StatusCodes.OK).json({
+  product
+ })
 }
 
 //updateProduct
 const updateProduct = async (req, res) => {
- res.send('update product')
+ const {
+  id: productId
+ } = req.params
+ const product = await Product.findOneAndUpdate({
+   _id: productId
+  },
+  req.body, {
+   new: true,
+   runValidators: true
+  }
+ )
+ if (!product) {
+  throw new CustomError.NotFoundError(`No product with id : ${productId}`)
+ }
+
+ res.status(StatusCodes.OK).json({
+  product
+ })
 }
 
 //deleteProduct
 const deleteProduct = async (req, res) => {
- res.send('delete product')
+ const {
+  id: productId
+ } = req.params
+ const product = await Product.findByIdAndRemove({
+  _id: productId
+ })
+ if (!product) {
+  throw new CustomError.NotFoundError(`No product with id : ${productId}`)
+ }
+
+
+ res.status(StatusCodes.OK).json({
+  msg: 'Success! Product removed'
+ })
 }
 
 //uploadImage
--- BackEnd/routes/Product.js
+++ BackEnd/routes/Product.js
@@ -16,7 +16,6 @@ const {
  authorizePermissions
 } = require('../middleware/authenticateUser.js')
 
-
 //............
 //App
 //............
