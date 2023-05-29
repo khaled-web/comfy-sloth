66daea3be8a83c3c1d4ff1b94ff2563da9d0064b
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-05-29 11:19:49 +0200

    finish the project

--- FrontEnd/src/pages/CheckoutPage.js
+++ FrontEnd/src/pages/CheckoutPage.js
@@ -6,13 +6,23 @@ import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 import Wrapper from '../assets/Wrapper/CheckoutStyled'
 const CheckoutPage = () => {
+  const {cart}=useCartContext()
   return (
     <main>
       <Navbar/>
       <Sidebar/>
       <PageHero title='checkout'/>
       <Wrapper className='page'>
-        <h2>checkout here</h2>
+        {cart.length<1?(
+          <div className="empty">
+            <h2>your cart is empty</h2>
+            <Link to='/products' className='btn'>
+            fill it
+            </Link>
+          </div>
+        ):(
+          <StripeCheckout/>
+        )}
       </Wrapper>
       <Footer/>
     </main>
